b7179c68ba0bf2ac6c2995b580aeda6cec38feb1
RashikFarhan <142752522+RashikFarhan@users.noreply.github.com>
2024-09-11 12:27:14 -0700

    Update read more main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -254,5 +254,16 @@
 			});
 
 		}
+	//Read More button
+		const readMoreBtn = document.querySelector('.read-more-btn');
+		const text = document.querySelector('.text');
 
+		readMoreBtn.addEventListener('click', (e) => {
+			text.classList.toggle('show-more');
+			if(readMoreBtn.innerText === 'Read More') {
+				readMoreBtn.innerText = 'Read Less';
+			} else {
+				readMoreBtn.innerText = 'Read More';
+			}
+		})
 })(jQuery);
